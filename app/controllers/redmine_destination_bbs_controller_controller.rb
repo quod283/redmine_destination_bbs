b45b567f36a0d6d904ead24449f047e3fc9011c0
tomioka <rm150461-8239@tbz.t-com.ne.jp>
2020-06-16 23:32:29 +0900

    Ver0.0.2 ・コメント登録機能追加 ・年休登録仕様変更→年休に限りカレンダーのどの日付でも登録/更新可能 ・en.yml一部対応 ・その他微修正

--- app/controllers/redmine_destination_bbs_controller_controller.rb
+++ app/controllers/redmine_destination_bbs_controller_controller.rb
@@ -7,22 +7,27 @@ class RedmineDestinationBbsControllerController < ApplicationController
     if (@search_params.blank?)
       @destination_bbs = RedmineDestinationBbsModel.where(registration_date: Date.today)
       @search_params_date = Date.today
+      @search_params[:registration_date] = Date.today
     else
       @destination_bbs = RedmineDestinationBbsModel.search(@search_params)
       @search_params_date = @search_params[:registration_date]
     end
     @users = User.select('id', 'lastname', 'firstname')
     @user_id = User.current.attributes["id"]
-    @destination_bbs_id = RedmineDestinationBbsModel.where(user_id: @user_id, registration_date: Date.today).select('id')
+    @destination_bbs_id = RedmineDestinationBbsModel.where(user_id: @user_id, registration_date: @search_params[:registration_date]).select('id')
   end
 
   def create
     @destination_bbs = RedmineDestinationBbsModel.new(params[:destination_bbs])
     @destination_bbs.user_id = params[:user_id]
     @destination_bbs.destination = params[:destination]
-    @destination_bbs.registration_date = Date.today
-    @destination_bbs.start_time = params[:start_time]
-
+    if params[:destination] == l(:button_holiday)
+      @destination_bbs.registration_date = params[:registration_date]
+    else
+      @destination_bbs.registration_date = Date.today
+      @destination_bbs.start_time = params[:start_time]
+    end
+    
     if @destination_bbs.save
       flash[:notice] = l(:notice_successful_create)
       redirect_back(fallback_location: {:controller => 'redmine_destination_bbs_controller', :action => 'index'})
@@ -30,11 +35,30 @@ class RedmineDestinationBbsControllerController < ApplicationController
   end
 
   def update
-    @destination_bbs = RedmineDestinationBbsModel.where(user_id: params[:user_id], registration_date: Date.today)
-    if @destination_bbs.update(destination: params[:destination], end_time: params[:end_time])
-      flash[:notice] = l(:notice_successful_update)
-      redirect_back(fallback_location: {:controller => 'redmine_destination_bbs_controller', :action => 'index'})
-    end 
+    if params[:comment].present?
+      @destination_bbs = RedmineDestinationBbsModel.where(user_id: params[:user_id], registration_date: params[:registration_date])
+      if @destination_bbs.update(comment: params[:comment])
+        flash[:notice] = l(:notice_successful_update)
+        redirect_back(fallback_location: {:controller => 'redmine_destination_bbs_controller', :action => 'index'})
+      end
+    else
+      if params[:destination] == l(:button_holiday)
+        @destination_bbs = RedmineDestinationBbsModel.where(user_id: params[:user_id], registration_date: params[:registration_date])
+      else
+        @destination_bbs = RedmineDestinationBbsModel.where(user_id: params[:user_id], registration_date: Date.today)
+      end
+      if params[:end_time].present?
+        if @destination_bbs.update(destination: params[:destination], end_time: params[:end_time])
+          flash[:notice] = l(:notice_successful_update)
+          redirect_back(fallback_location: {:controller => 'redmine_destination_bbs_controller', :action => 'index'})
+        end
+      else
+        if @destination_bbs.update(destination: params[:destination])
+          flash[:notice] = l(:notice_successful_update)
+          redirect_back(fallback_location: {:controller => 'redmine_destination_bbs_controller', :action => 'index'})
+        end
+      end
+    end
   end
 
   private
--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 Redmine::Plugin.register :redmine_destination_bbs do
-  name 'Redmine Destination Bbs plugin'
+  name 'Redmine Destination BBS plugin'
   author 'Trial Taro'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
   url ''
   author_url ''
   menu :top_menu, :redmine_destination_bbs, 
   {:controller => 'redmine_destination_bbs_controller', :action => 'index'},
-  :caption => '行先掲示板'
+  :caption => :title_destination_bbs
 end
